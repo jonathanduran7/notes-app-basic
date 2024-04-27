8df3da824f5021a4406de94211e6a175436b8bdd
jonathanduran7 <joniduran624@gmail.com>
2024-04-26 23:44:06 -0300

    empty notes messages

---
+++ app/notes/components/empty-notes.tsx
@@ -0,0 +1,9 @@
+export const EmptyList = () => {
+    return (
+        <div className="flex-grow p-5">
+            <div className="flex justify-center items-center h-full" >
+                <p className="text-slate-400">No hay notas</p>
+            </div >
+        </div>
+    )
+}
