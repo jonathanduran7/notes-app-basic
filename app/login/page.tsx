a5abf906f3e8a877fa13330ed15a29ba34aeeef8
jonathanduran7 <joniduran624@gmail.com>
2024-04-26 23:57:12 -0300

    rename button

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -52,7 +52,7 @@ export default function Login() {
                                 type="submit"
                                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mt-4"
                             >
-                                Submit
+                                Ingresar
                             </button>
                         </div>
                     </Form>
