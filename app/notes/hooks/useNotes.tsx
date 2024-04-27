3b3f99171c38129e65c75a55a1dd39454fc640d6
jonathanduran7 <joniduran624@gmail.com>
2024-04-27 00:11:37 -0300

    add localStorage support to useNotes hook

--- app/notes/hooks/useNotes.tsx
+++ app/notes/hooks/useNotes.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Note } from "../page";
 
 export const UseNotes = () => {
@@ -7,8 +7,20 @@ export const UseNotes = () => {
     const addNote = (note: Omit<Note, "id">) => {
         const id = Math.random().toString(36).substr(2, 9)
         const newNote = { ...note, id }
+        localStorage.setItem('notes', JSON.stringify([...notes, newNote]))
         setNotes([...notes, newNote])
     }
+
+    const getNotes = () => {
+        const notes = localStorage.getItem('notes')
+        if (notes) {
+            setNotes(JSON.parse(notes))
+        }
+    }
+
+    useEffect(() => {
+        getNotes()
+    }, [])
 
     return { notes, addNote }
 
