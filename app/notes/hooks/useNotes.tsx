3a50855225a9f2c19ea8bcd673232589cc8c61b4
jonathanduran7 <joniduran624@gmail.com>
2024-04-27 14:29:59 -0300

    modified useNotes hook

--- app/notes/hooks/useNotes.tsx
+++ app/notes/hooks/useNotes.tsx
@@ -1,27 +1,6 @@
-import { useEffect, useState } from "react";
-import { Note } from "../page";
+import { useContext } from "react";
+import { NoteContext } from "@/app/context/notes/notes.context";
 
-export const UseNotes = () => {
-    const [notes, setNotes] = useState<Note[]>([])
-
-    const addNote = (note: Omit<Note, "id">) => {
-        const id = Math.random().toString(36).substr(2, 9)
-        const newNote = { ...note, id }
-        localStorage.setItem('notes', JSON.stringify([...notes, newNote]))
-        setNotes([...notes, newNote])
-    }
-
-    const getNotes = () => {
-        const notes = localStorage.getItem('notes')
-        if (notes) {
-            setNotes(JSON.parse(notes))
-        }
-    }
-
-    useEffect(() => {
-        getNotes()
-    }, [])
-
-    return { notes, addNote }
-
+export const useNotes = () => {
+    return useContext(NoteContext)
 }
